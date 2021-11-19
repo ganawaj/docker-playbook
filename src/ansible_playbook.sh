85af06cf230efcc41e8e0a9e01fd3fc7bfeefe62
Ganawa Juanah <ganawa@juanah.com>
2021-11-19 09:52:45 -0600

    add check for vault token

--- src/ansible_playbook.sh
+++ src/ansible_playbook.sh
@@ -9,9 +9,12 @@ if [ -f /ansible/playbooks/roles/requirements.yml ]; then
     ansible-galaxy install -r /ansible/playbooks/roles/requirements.yml
 fi
 
-
-ssh-keygen -q -t rsa -C "$SSH_CERT_USER" -N '' -f $HOME/.ssh/id_rsa
-vault write -field=signed_key "$VAULT_SSH_SIGNER_PATH" public_key=@$HOME/.ssh/id_rsa.pub > $HOME/.ssh/id_rsa-cert.pub
-
+if [[ -z "${VAULT_TOKEN}" ]]; then
+    echo "No vault token found. Please set VAULT_TOKEN env to use ssh certificate signing"
+else
+    echo "VAULT_TOKEN found, generating and signing ssh keys"
+    ssh-keygen -q -t rsa -C "$SSH_CERT_USER" -N '' -f $HOME/.ssh/id_rsa
+    vault write -field=signed_key "$VAULT_SSH_SIGNER_PATH" public_key=@$HOME/.ssh/id_rsa.pub > $HOME/.ssh/id_rsa-cert.pub
+fi
 
 ansible-playbook "$@"
